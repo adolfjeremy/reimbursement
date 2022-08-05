4e10af2928a7525546ffd333a4093d28206b6036
adolfjeremy <adolf.jer@gmail.com>
2022-08-05 23:20:05 +0700

    fix add button on account setting page

--- resources/views/pages/user.blade.php
+++ resources/views/pages/user.blade.php
@@ -12,12 +12,53 @@
                 <h1>User Settings</h1>
                 </div>
             </div>
+            <div class="row">
+                <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                    <div class="modal-dialog modal-dialog-centered">
+                        <div class="modal-content">
+                            <div class="modal-header">
+                                <h5 class="modal-title" id="exampleModalLabel">Add Expense</h5>
+                                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                            </div>
+                            <div class="modal-body">
+                                <form action="{{ route('monthly-report-store') }}" method="POST" enctype="multipart/form-data">
+                                    @csrf
+                                    <div class="mb-3">
+                                        <label for="name" class="col-form-label">Name:</label>
+                                        <input name="name" type="text" class="form-control" id="name" required>
+                                    </div>
+                                    <div class="mb-3">
+                                        <label for="amount" class="col-form-label">Price:</label>
+                                        <input name="amount" type="number" class="form-control" id="amount" required>
+                                    </div>
+                                    <div class="mb-3">
+                                        <label for="entry_date" class="col-form-label">Date:</label>
+                                        <input name="entry_date" type="date" class="form-control" id="entry_date" required>
+                                    </div>
+                                    <div class="mb-3">
+                                        <label for="receipt" class="col-form-label">Receipt:</label>
+                                        <input name="receipt" type="file" class="form-control" id="receipt" required>
+                                    </div>
+                                    <div class="mb-3">
+                                        <label for="description" class="col-form-label">Description:</label>
+                                        <textarea name="description" class="form-control" id="description"></textarea>
+                                    </div>
+                                    <div class="modal-footer">
+                                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                                        <button type="submit" class="btn btn-primary">Save</button>
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
             <div class="name_wrapper d-flex align-items-center">
                 <div class="profile_picture" style="background-image: url('{{ asset("storage/" . $item->photo) }}')"></div>
-                <a type="button" class="ms-2" data-bs-toggle="modal" data-bs-target="#exampleModal">
+                <a type="button" class="ms-2" data-bs-toggle="modal" data-bs-target="#exampleModal2">
                 Change Profile Photo
                 </a>
-                <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                <div class="modal fade" id="exampleModal2" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
                     <div class="modal-dialog modal-dialog-centered">
                         <div class="modal-content">
                             <div class="modal-header">
