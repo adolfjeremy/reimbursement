0e70e2f3ff09cfb06842bb745adb30f073441f84
adolfjeremy <adolf.jer@gmail.com>
2022-08-10 20:57:07 +0700

    fix style

--- resources/views/includes/styles.blade.php
+++ resources/views/includes/styles.blade.php
@@ -1,2 +1,2 @@
 <link rel="stylesheet" href="/assets/vendor/bootstrap-5.2.0-beta1-dist/css/bootstrap.min.css">
-<link rel="stylesheet" href="/assets/styles/style2.css">
+<link rel="stylesheet" href="/assets/styles/style3.css">
